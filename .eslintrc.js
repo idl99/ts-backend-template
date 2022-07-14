120a7f94ed567b4aca59142a770d384b3e2118cb
Ihan Dilnath <ihandilnath2012@gmail.com>
2022-07-14 23:37:23 +0530

    update eslint and tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,26 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: ['./tsconfig.json'],
+  },
+  plugins: ['@typescript-eslint'],
+  extends: [
       'eslint:recommended',
       'plugin:@typescript-eslint/recommended',
-    ],
-  };
+      'plugin:@typescript-eslint/recommended-requiring-type-checking',
+  ],
+  rules: {
+      '@typescript-eslint/explicit-module-boundary-types': 'off',
+  },
+  overrides: [
+      {
+          // enable the rule specifically for TypeScript files
+          files: ['*.ts', '*.tsx'],
+          rules: {
+              '@typescript-eslint/explicit-module-boundary-types': ['error'],
+          },
+      },
+  ],
+}
