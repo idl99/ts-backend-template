87b6eebd7ffed0caece556cacc9bdadaf5aed9d8
Ihan Dilnath <ihandilnath2012@gmail.com>
2022-07-15 00:29:45 +0530

    update eslint config for jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,31 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-      tsconfigRootDir: __dirname,
-      project: ['./tsconfig.json'],
-  },
-  plugins: ['@typescript-eslint'],
-  extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'plugin:@typescript-eslint/recommended-requiring-type-checking',
-  ],
-  rules: {
-      '@typescript-eslint/explicit-module-boundary-types': 'off',
-  },
-  overrides: [
-      {
-          // enable the rule specifically for TypeScript files
-          files: ['*.ts', '*.tsx'],
-          rules: {
-              '@typescript-eslint/explicit-module-boundary-types': ['error'],
-          },
-      },
-  ],
-}
+	root: true,
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: ['./tsconfig.json'],
+	},
+	plugins: ['@typescript-eslint'],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+	],
+	rules: {
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+	},
+	overrides: [
+		{
+			// enable the rule specifically for TypeScript files
+			files: ['*.ts', '*.tsx'],
+			rules: {
+				'@typescript-eslint/explicit-module-boundary-types': ['error'],
+			},
+		},
+		{
+			files: ['test/**'],
+			plugins: ['jest'],
+			extends: ['plugin:jest/recommended'],
+		},
+	],
+};
